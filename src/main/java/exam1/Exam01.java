e60951893586e656984c8630d3ddd7ea124b09dd
kulbandita <kulbandita@gmail.com>
2017-07-21 15:39:42 +0700

    player_A_win_1_should_Fifteen_Love() is done

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -2,17 +2,25 @@ package exam1;
 
 public class Exam01 {
 
+    private int scorePlayerA = 0;
+
     public Exam01(String firstPerson, String secondPerson) {
     }
 
-
     public void playerAWin() {
+        scorePlayerA++;
     }
 
     public void playerBWin() {
     }
 
     public String getScore() {
+        int score = 0;
+
+        if(scorePlayerA == 1){
+            return "Fifteen-Love";
+        }
+
         return "Love-All";
     }
 
